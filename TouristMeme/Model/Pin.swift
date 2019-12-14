422f8a0a823b29f785039a3fc8c670119954a833
Saad <s.alajlan10@gmail.com>
2019-12-14 22:14:07 +0300

    added pin

---
+++ TouristMeme/Model/Pin.swift
@@ -0,0 +1,29 @@
+//
+//  Pin.swift
+//  TouristMeme
+//
+//  Created by Saad on 12/13/19.
+//  Copyright © 2019 saad. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+public class Pin: NSManagedObject {
+    
+    convenience init(lat: Double, long: Double,  context: NSManagedObjectContext) {
+        
+        if let ent = NSEntityDescription.entity(forEntityName: "Pin", in: context) {
+            self.init(entity: ent, insertInto: context)
+            self.lat = lat
+            self.long = long
+            
+        } else {
+            fatalError("Unable to find Entity name!")
+        }
+    }
+    
+    
+    
+}
